7971eb73d69602a8356536cc2a132946a0b1eec9
Doug <doug@bfjmedia.com.au>
2021-12-10 12:52:40 +1000

    cosmetic fixes

--- src/pages/Proposals/Main.vue
+++ src/pages/Proposals/Main.vue
@@ -104,7 +104,7 @@ export default {
   }
   @media screen and (max-width: 375px) and (min-width: 200px) {
     .container {
-      width: 92vw !important;
+      width: 91vw !important;
     }  
     .proposal-texts {
       font-size: 14px;
